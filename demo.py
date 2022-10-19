6bf32099652ca5f853c6fa69403485009956f25c
chLi <565353780@qq.com>
2022-10-19 20:40:30 +0800

    add manage texture demo func

--- demo.py
+++ demo.py
@@ -9,10 +9,15 @@ from texture_synthesis.Demo.texture_generator import \
     demo_trans_all as demo_trans_image_to_all_texture, \
     demo_trans_folder_all as demo_trans_image_folder_to_all_texture
 
+from texture_synthesis.Demo.texture_manager import \
+    demo as demo_manage_texture
+
 if __name__ == "__main__":
     #  demo_generate_texture()
     #  demo_generate_repeat_texture()
     #  demo_trans_image_to_texture()
     #  demo_trans_image_folder_to_texture()
     #  demo_trans_image_to_all_texture()
+
     demo_trans_image_folder_to_all_texture()
+    demo_manage_texture()
