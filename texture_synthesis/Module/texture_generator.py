bd922ac4719ab62d55d0d116507b3f192d91237f
chLi <565353780@qq.com>
2022-10-19 16:57:56 +0800

    add 4 new functions

--- texture_synthesis/Module/texture_generator.py
+++ texture_synthesis/Module/texture_generator.py
@@ -65,3 +65,35 @@ class TextureGenerator(object):
 
         image = (result * 255).astype(np.uint8)
         return image
+
+    def generateWidthRepeatTexture(self, image_path, print_progress=False):
+        texture = self.generateTexture(image_path, 1.0, (3, 1), print_progress)
+        width_split = int(texture.shape[1] / 3.0)
+        return texture[:, width_split:2 * width_split]
+
+    def generateHeightRepeatTexture(self, image_path, print_progress=False):
+        texture = self.generateTexture(image_path, 1.0, (1, 3), print_progress)
+        height_split = int(texture.shape[0] / 3.0)
+        return texture[height_split:2 * height_split, :]
+
+    def generateWidthAndHeightRepeatTexture(self,
+                                            image_path,
+                                            print_progress=False):
+        texture = self.generateTexture(image_path, 1.0, (3, 3), print_progress)
+        width_split = int(texture.shape[1] / 3.0)
+        height_split = int(texture.shape[0] / 3.0)
+        return texture[height_split:2 * height_split,
+                       width_split:2 * width_split]
+
+    def generateRepeatTexture(self,
+                              image_path,
+                              width_repeat=True,
+                              height_repeat=True,
+                              print_progress=False):
+        if width_repeat:
+            if height_repeat:
+                return self.generateWidthAndHeightRepeatTexture(
+                    image_path, print_progress)
+            return self.generateWidthRepeatTexture(image_path, print_progress)
+        if height_repeat:
+            return self.generateHeightRepeatTexture(image_path, print_progress)
