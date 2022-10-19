9ee6e07e91d06b637cc7d798a103e9555f5637e3
chLi <565353780@qq.com>
2022-10-19 16:32:48 +0800

    use percent instead of block size

--- texture_synthesis/Demo/texture_generator.py
+++ texture_synthesis/Demo/texture_generator.py
@@ -8,12 +8,13 @@ from texture_synthesis.Module.texture_generator import TextureGenerator
 
 def demo():
     image_path = "./data/input1.jpg"
-    block_size = 192
-    num_block = (4, 3)
+    image_path = "/home/chli/chLi/texture/flower.png"
+    patch_sample_percent = 1.0
+    num_block = (3, 3)
     print_progress = True
 
     texture_generator = TextureGenerator()
-    image = texture_generator.generateTexture(image_path, block_size,
+    image = texture_generator.generateTexture(image_path, patch_sample_percent,
                                               num_block, print_progress)
     cv2.imshow("image", image)
     cv2.waitKey(0)
