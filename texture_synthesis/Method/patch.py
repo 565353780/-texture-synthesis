d1e870692397bed71a973d26f485053ff5c12e0d
chLi <565353780@qq.com>
2022-10-19 16:40:19 +0800

    now will support any image size

--- texture_synthesis/Method/patch.py
+++ texture_synthesis/Method/patch.py
@@ -8,42 +8,43 @@ import numpy as np
 def getRandomPatch(texture, block_size):
     h, w = texture.shape[:2]
 
-    if h == block_size:
+    if h == block_size[1]:
         i = 0
     else:
-        i = np.random.randint(h - block_size)
+        i = np.random.randint(h - block_size[1])
 
-    if w == block_size:
+    if w == block_size[0]:
         j = 0
     else:
-        j = np.random.randint(w - block_size)
-    return texture[i:i + block_size, j:j + block_size]
+        j = np.random.randint(w - block_size[0])
+    return texture[i:i + block_size[1], j:j + block_size[0]]
 
 
 def getL2OverlapDiffError(patch, block_size, overlap, result, y, x):
     error = 0
     if x > 0:
-        left = patch[:, :overlap] - result[y:y + block_size, x:x + overlap]
+        left = patch[:, :overlap] - result[y:y + block_size[1],
+                                           x:x + overlap[0]]
         error += np.sum(left**2)
 
     if y > 0:
-        up = patch[:overlap, :] - result[y:y + overlap, x:x + block_size]
+        up = patch[:overlap, :] - result[y:y + overlap[1], x:x + block_size[0]]
         error += np.sum(up**2)
 
     if x > 0 and y > 0:
-        corner = patch[:overlap, :overlap] - result[y:y + overlap,
-                                                    x:x + overlap]
+        corner = patch[:overlap[1], :overlap[0]] - result[y:y + overlap[1],
+                                                          x:x + overlap[0]]
         error -= np.sum(corner**2)
     return error
 
 
 def getRandomBestPatch(texture, block_size, overlap, result, y, x):
     h, w = texture.shape[:2]
-    errors = np.zeros((h - block_size, w - block_size))
+    errors = np.zeros((h - block_size[1], w - block_size[0]))
 
-    for i in range(h - block_size):
-        for j in range(w - block_size):
-            patch = texture[i:i + block_size, j:j + block_size]
+    for i in range(h - block_size[1]):
+        for j in range(w - block_size[0]):
+            patch = texture[i:i + block_size[1], j:j + block_size[0]]
             errors[i, j] = getL2OverlapDiffError(patch, block_size, overlap,
                                                  result, y, x)
 
@@ -51,7 +52,7 @@ def getRandomBestPatch(texture, block_size, overlap, result, y, x):
         i, j = np.unravel_index(np.argmin(errors), errors.shape)
     else:
         i, j = 0, 0
-    return texture[i:i + block_size, j:j + block_size]
+    return texture[i:i + block_size[1], j:j + block_size[0]]
 
 
 def getMinCutPath(errors):
@@ -87,13 +88,13 @@ def getMinCutPatch(patch, overlap, result, y, x):
     minCut = np.zeros_like(patch, dtype=bool)
 
     if x > 0:
-        left = patch[:, :overlap] - result[y:y + dy, x:x + overlap]
+        left = patch[:, :overlap[0]] - result[y:y + dy, x:x + overlap[0]]
         leftL2 = np.sum(left**2, axis=2)
         for i, j in enumerate(getMinCutPath(leftL2)):
             minCut[i, :j] = True
 
     if y > 0:
-        up = patch[:overlap, :] - result[y:y + overlap, x:x + dx]
+        up = patch[:overlap[1], :] - result[y:y + overlap[1], x:x + dx]
         upL2 = np.sum(up**2, axis=2)
         for j, i in enumerate(getMinCutPath(upL2.T)):
             minCut[:i, j] = True
