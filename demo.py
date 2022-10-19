ccba7a5b4f47a09a6ccc95010c0ea30ac3dabe3a
chLi <565353780@qq.com>
2022-10-19 19:46:07 +0800

    add 2 more new demo functions

--- demo.py
+++ demo.py
@@ -5,10 +5,14 @@ from texture_synthesis.Demo.texture_generator import \
     demo as demo_generate_texture, \
     demo_repeat as demo_generate_repeat_texture, \
     demo_trans as demo_trans_image_to_texture, \
-    demo_trans_folder as demo_trans_image_folder_to_texture
+    demo_trans_folder as demo_trans_image_folder_to_texture, \
+    demo_trans_all as demo_trans_image_to_all_texture, \
+    demo_trans_folder_all as demo_trans_image_folder_to_all_texture
 
 if __name__ == "__main__":
     #  demo_generate_texture()
     #  demo_generate_repeat_texture()
     #  demo_trans_image_to_texture()
-    demo_trans_image_folder_to_texture()
+    #  demo_trans_image_folder_to_texture()
+    #  demo_trans_image_to_all_texture()
+    demo_trans_image_folder_to_all_texture()
--- texture_synthesis/Demo/texture_generator.py
+++ texture_synthesis/Demo/texture_generator.py
@@ -50,6 +50,33 @@ def demo_repeat():
 def demo_trans():
     image_file_path = "/home/chli/chLi/texture/Image/flower.png"
     save_texture_folder_path = "/home/chli/chLi/texture/Texture/flower/"
+    width_repeat = True
+    height_repeat = True
+
+    texture_generator = TextureGenerator()
+    texture_generator.transImageToRepeatTexture(image_file_path,
+                                                save_texture_folder_path,
+                                                width_repeat, height_repeat)
+    return True
+
+
+def demo_trans_folder():
+    image_folder_path = "/home/chli/chLi/texture/Image/"
+    save_texture_folder_path = "/home/chli/chLi/texture/Texture/"
+    width_repeat = True
+    height_repeat = True
+    print_progress = True
+
+    texture_generator = TextureGenerator()
+    texture_generator.transImageFolderToRepeatTexture(
+        image_folder_path, save_texture_folder_path, width_repeat,
+        height_repeat, print_progress)
+    return True
+
+
+def demo_trans_all():
+    image_file_path = "/home/chli/chLi/texture/Image/flower.png"
+    save_texture_folder_path = "/home/chli/chLi/texture/Texture/flower/"
 
     texture_generator = TextureGenerator()
     texture_generator.transImageToAllRepeatTexture(image_file_path,
@@ -57,7 +84,7 @@ def demo_trans():
     return True
 
 
-def demo_trans_folder():
+def demo_trans_folder_all():
     image_folder_path = "/home/chli/chLi/texture/Image/"
     save_texture_folder_path = "/home/chli/chLi/texture/Texture/"
     print_progress = True
