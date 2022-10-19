b53329f586540c809e1939ccfcab5a1e3aa48aae
chLi <565353780@qq.com>
2022-10-19 16:10:13 +0800

    remove unused import, use opencv as default

--- texture_synthesis/Module/texture_generator.py
+++ texture_synthesis/Module/texture_generator.py
@@ -2,49 +2,60 @@
 # -*- coding: utf-8 -*-
 
 import os
+import cv2
 import heapq
 import numpy as np
 from tqdm import tqdm
-from PIL import Image
-from skimage import util
 
 
 def randomPatch(texture, block_size):
     h, w, _ = texture.shape
-    i = np.random.randint(h - block_size)
-    j = np.random.randint(w - block_size)
+
+    if h == block_size:
+        i = 0
+    else:
+        i = np.random.randint(h - block_size)
+
+    if w == block_size:
+        j = 0
+    else:
+        j = np.random.randint(w - block_size)
 
     return texture[i:i + block_size, j:j + block_size]
 
 
-def L2OverlapDiff(patch, block_size, overlap, res, y, x):
+def L2OverlapDiff(patch, block_size, overlap, result, y, x):
     error = 0
     if x > 0:
-        left = patch[:, :overlap] - res[y:y + block_size, x:x + overlap]
+        left = patch[:, :overlap] - result[y:y + block_size, x:x + overlap]
         error += np.sum(left**2)
 
     if y > 0:
-        up = patch[:overlap, :] - res[y:y + overlap, x:x + block_size]
+        up = patch[:overlap, :] - result[y:y + overlap, x:x + block_size]
         error += np.sum(up**2)
 
     if x > 0 and y > 0:
-        corner = patch[:overlap, :overlap] - res[y:y + overlap, x:x + overlap]
+        corner = patch[:overlap, :overlap] - result[y:y + overlap,
+                                                    x:x + overlap]
         error -= np.sum(corner**2)
 
     return error
 
 
-def randomBestPatch(texture, block_size, overlap, res, y, x):
+def randomBestPatch(texture, block_size, overlap, result, y, x):
     h, w, _ = texture.shape
     errors = np.zeros((h - block_size, w - block_size))
 
     for i in range(h - block_size):
         for j in range(w - block_size):
             patch = texture[i:i + block_size, j:j + block_size]
-            e = L2OverlapDiff(patch, block_size, overlap, res, y, x)
+            e = L2OverlapDiff(patch, block_size, overlap, result, y, x)
             errors[i, j] = e
 
-    i, j = np.unravel_index(np.argmin(errors), errors.shape)
+    if errors.shape[0] > 0 and errors.shape[1] > 0:
+        i, j = np.unravel_index(np.argmin(errors), errors.shape)
+    else:
+        i, j = 0, 0
     return texture[i:i + block_size, j:j + block_size]
 
 
@@ -74,24 +85,24 @@ def minCutPath(errors):
                     seen.add((curDepth, nextIndex))
 
 
-def minCutPatch(patch, overlap, res, y, x):
+def minCutPatch(patch, overlap, result, y, x):
     patch = patch.copy()
     dy, dx, _ = patch.shape
     minCut = np.zeros_like(patch, dtype=bool)
 
     if x > 0:
-        left = patch[:, :overlap] - res[y:y + dy, x:x + overlap]
+        left = patch[:, :overlap] - result[y:y + dy, x:x + overlap]
         leftL2 = np.sum(left**2, axis=2)
         for i, j in enumerate(minCutPath(leftL2)):
             minCut[i, :j] = True
 
     if y > 0:
-        up = patch[:overlap, :] - res[y:y + overlap, x:x + dx]
+        up = patch[:overlap, :] - result[y:y + overlap, x:x + dx]
         upL2 = np.sum(up**2, axis=2)
         for j, i in enumerate(minCutPath(upL2.T)):
             minCut[:i, j] = True
 
-    np.copyto(patch, res[y:y + dy, x:x + dx], where=minCut)
+    np.copyto(patch, result[y:y + dy, x:x + dx], where=minCut)
 
     return patch
 
@@ -101,41 +112,48 @@ class TextureGenerator(object):
     def __init__(self):
         return
 
-    def generateTexture(self, image_path, block_size, num_block, mode, print_progress=False):
+    def generateTexture(self,
+                        image_path,
+                        block_size,
+                        num_block,
+                        print_progress=False):
         assert os.path.exists(image_path)
 
-        texture = Image.open(image_path)
-        texture = util.img_as_float(texture)
+        texture = cv2.imread(image_path)
+        texture = texture / 255.0
 
         overlap = block_size // 6
-        num_blockHigh, num_blockWide = num_block
 
-        h = (num_blockHigh * block_size) - (num_blockHigh - 1) * overlap
-        w = (num_blockWide * block_size) - (num_blockWide - 1) * overlap
+        block_width_num, block_height_num = num_block
 
-        res = np.zeros((h, w, texture.shape[2]))
+        w = (block_width_num * block_size) - (block_width_num - 1) * overlap
+        h = (block_height_num * block_size) - (block_height_num - 1) * overlap
+        print("image size:", w, h)
 
-        for_data = range(num_blockHigh)
+        result = np.zeros((h, w, texture.shape[2]))
+
+        block_num = block_width_num * block_height_num
+
+        for_data = range(block_num)
         if print_progress:
             print("[INFO][TextureGenerator::generateTexture]")
             print("\t start generate texture...")
             for_data = tqdm(for_data)
-        for i in for_data:
-            for j in range(num_blockWide):
-                y = i * (block_size - overlap)
-                x = j * (block_size - overlap)
+        for block_idx in for_data:
+            width_idx = block_idx // block_height_num
+            height_idx = block_idx % block_height_num
 
-                if i == 0 and j == 0 or mode == "Random":
-                    patch = randomPatch(texture, block_size)
-                elif mode == "Best":
-                    patch = randomBestPatch(texture, block_size, overlap, res,
-                                            y, x)
-                elif mode == "Cut":
-                    patch = randomBestPatch(texture, block_size, overlap, res,
-                                            y, x)
-                    patch = minCutPatch(patch, overlap, res, y, x)
+            x = width_idx * (block_size - overlap)
+            y = height_idx * (block_size - overlap)
 
-                res[y:y + block_size, x:x + block_size] = patch
+            if width_idx == 0 and height_idx == 0:
+                patch = randomPatch(texture, block_size)
+            else:
+                patch = randomBestPatch(texture, block_size, overlap, result,
+                                        y, x)
+                patch = minCutPatch(patch, overlap, result, y, x)
 
-        image = Image.fromarray((res * 255).astype(np.uint8))
+            result[y:y + block_size, x:x + block_size] = patch
+
+        image = (result * 255).astype(np.uint8)
         return image
