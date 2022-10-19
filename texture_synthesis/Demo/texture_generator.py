8302f4534091ec80694d76ee6d52f6eba229fe08
chLi <565353780@qq.com>
2022-10-19 17:07:10 +0800

    use image instead of image_path as input

--- texture_synthesis/Demo/texture_generator.py
+++ texture_synthesis/Demo/texture_generator.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import cv2
 
 from texture_synthesis.Module.texture_generator import TextureGenerator
 
 
 def demo():
-    image_path = "./data/input1.jpg"
-    image_path = "/home/chli/chLi/texture/flower.png"
+    image_file_path = "./data/input1.jpg"
+    image_file_path = "/home/chli/chLi/texture/flower.png"
     patch_sample_percent = 0.3
-    num_block = (4, 4)
+    block_num_list = (4, 4)
     print_progress = True
+
+    assert os.path.exists(image_file_path)
+    image = cv2.imread(image_file_path)
 
     texture_generator = TextureGenerator()
     generated_texture = texture_generator.generateTexture(
-        image_path, patch_sample_percent, num_block, print_progress)
+        image, patch_sample_percent, block_num_list, print_progress)
 
     width_repeat_texture = texture_generator.generateRepeatTexture(
-        image_path, height_repeat=False, print_progress=print_progress)
+        image, height_repeat=False, print_progress=print_progress)
 
     height_repeat_texture = texture_generator.generateRepeatTexture(
-        image_path, width_repeat=False, print_progress=print_progress)
+        image, width_repeat=False, print_progress=print_progress)
 
     repeat_texture = texture_generator.generateRepeatTexture(
-        image_path, print_progress)
+        image, print_progress)
 
     cv2.imshow("generated_texture", generated_texture)
     cv2.imshow("width_repeat_texture", width_repeat_texture)
