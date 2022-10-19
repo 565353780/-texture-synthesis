399d982897361c2563a9e5a9e04bfa421b769cd5
chLi <565353780@qq.com>
2022-10-19 16:10:03 +0800

    use opencv as default, remove unused mode param

--- texture_synthesis/Demo/texture_generator.py
+++ texture_synthesis/Demo/texture_generator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import cv2
+
 from texture_synthesis.Module.texture_generator import TextureGenerator
 
 
 def demo():
     image_path = "./data/input1.jpg"
-    block_size = 60
-    num_block = 8
-    mode = 'Best'  # Best or Cut
+    block_size = 192
+    num_block = (4, 3)
     print_progress = True
 
     texture_generator = TextureGenerator()
     image = texture_generator.generateTexture(image_path, block_size,
-                                              (num_block, num_block), mode,
-                                              print_progress)
-    image.show()
+                                              num_block, print_progress)
+    cv2.imshow("image", image)
+    cv2.waitKey(0)
     return True
