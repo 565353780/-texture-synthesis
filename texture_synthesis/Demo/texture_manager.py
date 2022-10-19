a21f3888119ecb5b863d9810a900362b6d8ba1be
chLi <565353780@qq.com>
2022-10-19 20:40:22 +0800

    show all repeat results

--- texture_synthesis/Demo/texture_manager.py
+++ texture_synthesis/Demo/texture_manager.py
@@ -8,15 +8,31 @@ from texture_synthesis.Module.texture_manager import TextureManager
 
 
 def demo():
-    texture_file_path = "/home/chli/chLi/texture/Texture/flower/repeat.png"
-    repeat_size = [3, 2]
+    texture_folder_path = "/home/chli/chLi/texture/Texture/木纹/柚木/"
 
-    assert os.path.exists(texture_file_path)
+    width_texture_file_path = texture_folder_path + "width_repeat.png"
+    width_repeat_size = [3, 1]
+
+    height_texture_file_path = texture_folder_path + "height_repeat.png"
+    height_repeat_size = [1, 3]
+
+    texture_file_path = texture_folder_path + "repeat.png"
+    repeat_size = [3, 3]
+
+    assert os.path.exists(texture_folder_path)
+    width_texture = cv2.imread(width_texture_file_path)
+    height_texture = cv2.imread(height_texture_file_path)
     texture = cv2.imread(texture_file_path)
 
     texture_manager = TextureManager()
+    width_repeat_texture = texture_manager.renderRepeatTexture(
+        width_texture, width_repeat_size)
+    height_repeat_texture = texture_manager.renderRepeatTexture(
+        height_texture, height_repeat_size)
     repeat_texture = texture_manager.renderRepeatTexture(texture, repeat_size)
 
+    cv2.imshow("width_repeat_texture", width_repeat_texture)
+    cv2.imshow("height_repeat_texture", height_repeat_texture)
     cv2.imshow("repeat_texture", repeat_texture)
     cv2.waitKey(0)
     return True
