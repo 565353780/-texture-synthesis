80e0582ce00f22053058bc40bb801e5f300e2f09
chLi <565353780@qq.com>
2022-10-31 03:03:44 +0800

    update image file path, use simple repeat texture demo to debug

--- texture_synthesis/Demo/texture_generator.py
+++ texture_synthesis/Demo/texture_generator.py
@@ -8,7 +8,7 @@ from texture_synthesis.Module.texture_generator import TextureGenerator
 
 
 def demo():
-    image_file_path = "/home/chli/chLi/texture/图片/11.jpg"
+    image_file_path = "/home/chli/chLi/texture/Image/图片/11.jpg"
     patch_sample_percent_list = [0.9, 0.9]
     patch_overlap_percent_list = [0.9, 0.9]
     block_num_list = [3, 3]
@@ -31,23 +31,24 @@ def demo():
 
 
 def demo_repeat():
-    image_file_path = "/home/chli/chLi/texture/flower.png"
+    image_file_path = "/home/chli/chLi/texture/Image/图片/11.jpg"
 
     assert os.path.exists(image_file_path)
     image = cv2.imread(image_file_path)
 
     texture_generator = TextureGenerator()
-    width_repeat_texture = texture_generator.generateRepeatTexture(
-        image, height_repeat=False)
-
-    height_repeat_texture = texture_generator.generateRepeatTexture(
-        image, width_repeat=False)
 
     repeat_texture = texture_generator.generateRepeatTexture(image)
-
-    cv2.imshow("width_repeat_texture", width_repeat_texture)
-    cv2.imshow("height_repeat_texture", height_repeat_texture)
     cv2.imshow("repeat_texture", repeat_texture)
+
+    #  width_repeat_texture = texture_generator.generateRepeatTexture(
+    #  image, height_repeat=False)
+    #  cv2.imshow("width_repeat_texture", width_repeat_texture)
+
+    #  height_repeat_texture = texture_generator.generateRepeatTexture(
+    #  image, width_repeat=False)
+    #  cv2.imshow("height_repeat_texture", height_repeat_texture)
+
     cv2.waitKey(0)
     return True
 
