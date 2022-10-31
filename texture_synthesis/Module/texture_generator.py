56ad4f8e8b7b244db60afc803c931fc2673b3a48
chLi <565353780@qq.com>
2022-10-31 20:11:47 +0800

    simplify module

--- texture_synthesis/Module/texture_generator.py
+++ texture_synthesis/Module/texture_generator.py
@@ -4,11 +4,10 @@
 import os
 
 import cv2
-import numpy as np
 from tqdm import tqdm
 
 from texture_synthesis.Method.cut import getBlockImage
-from texture_synthesis.Method.patch import getMinCutPatch, getRandomBestPatch, getRandomPatch
+from texture_synthesis.Method.texture import generateTexture
 from texture_synthesis.Method.path import createFileFolder, renameFile
 
 
@@ -16,59 +15,6 @@ class TextureGenerator(object):
 
     def __init__(self):
         return
-
-    def generateTexture(self,
-                        image,
-                        patch_sample_percent_list,
-                        patch_overlap_percent_list,
-                        block_num_list,
-                        print_progress=False):
-        texture = image / 255.0
-
-        block_size = [
-            int(texture.shape[1 - i] * patch_sample_percent_list[i])
-            for i in range(2)
-        ]
-
-        overlap = [
-            int(block_size[i] * patch_overlap_percent_list[i])
-            for i in range(2)
-        ]
-
-        block_width_num, block_height_num = block_num_list
-
-        w = (block_width_num *
-             block_size[0]) - (block_width_num - 1) * overlap[0]
-        h = (block_height_num *
-             block_size[1]) - (block_height_num - 1) * overlap[1]
-
-        result = np.zeros((h, w, texture.shape[2]))
-
-        block_num = block_width_num * block_height_num
-
-        for_data = range(block_num)
-        if print_progress:
-            print("[INFO][TextureGenerator::generateTexture]")
-            print("\t start generate texture...")
-            for_data = tqdm(for_data)
-        for block_idx in for_data:
-            width_idx = block_idx // block_height_num
-            height_idx = block_idx % block_height_num
-
-            x = width_idx * (block_size[0] - overlap[0])
-            y = height_idx * (block_size[1] - overlap[1])
-
-            if width_idx == 0 and height_idx == 0:
-                patch = getRandomPatch(texture, block_size)
-            else:
-                patch = getRandomBestPatch(texture, block_size, overlap,
-                                           result, y, x)
-                patch, x_error, y_error = getMinCutPatch(patch, overlap, result, y, x)
-
-            result[y:y + block_size[1], x:x + block_size[0]] = patch
-
-        generated_texture = (result * 255).astype(np.uint8)
-        return generated_texture, block_size, overlap
 
     def generateRepeatTexture(self,
                               image,
@@ -85,7 +31,7 @@ class TextureGenerator(object):
             pre_patch_overlap_percent_list = [0.9, 0.9]
             pre_block_num_list = [2, 2]
 
-            pre_texture, _, _ = self.generateTexture(
+            pre_texture, _, _, _ = generateTexture(
                 image, pre_patch_sample_percent_list,
                 pre_patch_overlap_percent_list, pre_block_num_list,
                 print_progress)
@@ -109,7 +55,7 @@ class TextureGenerator(object):
             block_num_list[1] = 3
             height_block_range = [1, 2]
 
-        texture, block_size, overlap = self.generateTexture(
+        texture, block_size, overlap, _ = generateTexture(
             pre_texture, patch_sample_percent_list, patch_overlap_percent_list,
             block_num_list, print_progress)
 
