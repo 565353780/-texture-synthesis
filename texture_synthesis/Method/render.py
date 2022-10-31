96909fd3551cf3463108cac724a17f3a5070340f
chLi <565353780@qq.com>
2022-10-31 19:16:35 +0800

    update render effect

--- texture_synthesis/Method/render.py
+++ texture_synthesis/Method/render.py
@@ -6,16 +6,22 @@ import open3d as o3d
 
 
 def renderMinCutPatch(dist_map):
+    dist_max = np.max(dist_map)
+    dist_map = np.clip(dist_map, 0, dist_max / 4.0)
+
+    dist_max = np.max(dist_map)
     dist_array = dist_map.reshape(-1)
 
-    dist_max = np.max(dist_array)
     points = []
     for i in range(dist_map.shape[0]):
         for j in range(dist_map.shape[1]):
             points.append([i, j, 1000 * dist_map[i][j]])
     points = np.array(points)
+
     colors = np.zeros_like(points)
     colors[:, 0] = dist_array / dist_max
+    colors[:, 2] = 1 - (dist_array / dist_max)
+
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
     pcd.colors = o3d.utility.Vector3dVector(colors)
