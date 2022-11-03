778fc47f808bb4f005f45da6bd4ab1f424eb3ff7
chLi <565353780@qq.com>
2022-11-03 21:11:57 +0800

    remove unused demo functions

--- demo.py
+++ demo.py
@@ -2,32 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from texture_synthesis.Demo.texture_generator import \
-    demo as demo_generate_texture, \
     demo_repeat as demo_generate_repeat_texture, \
-    demo_trans as demo_trans_image_to_texture, \
-    demo_trans_folder as demo_trans_image_folder_to_texture, \
-    demo_trans_all as demo_trans_image_to_all_texture, \
     demo_trans_folder_all as demo_trans_image_folder_to_all_texture
-
-from texture_synthesis.Demo.rust_texture_generator import \
-    demo as demo_rust_generate_texture, \
-    demo_folder as demo_rust_generate_texture_folder
 
 from texture_synthesis.Demo.texture_manager import \
     demo as demo_manage_texture, \
     demo_folder as demo_manage_texture_folder
 
 if __name__ == "__main__":
-    #  demo_generate_texture()
-    demo_generate_repeat_texture()
-    exit()
-    #  demo_trans_image_to_texture()
-    #  demo_trans_image_folder_to_texture()
-    #  demo_trans_image_to_all_texture()
-    #  demo_trans_image_folder_to_all_texture()
+    #  demo_generate_repeat_texture()
+    demo_trans_image_folder_to_all_texture()
 
-    #  demo_rust_generate_texture()
-    demo_rust_generate_texture_folder()
-
-    demo_manage_texture()
+    #  demo_manage_texture()
     #  demo_manage_texture_folder()
