56fbc39dada380ee2b68941ce0ccb9befb446de4
chLi <565353780@qq.com>
2022-11-03 21:48:21 +0800

    remove manager demo, add render demo

--- demo.py
+++ demo.py
@@ -5,13 +5,11 @@ from texture_synthesis.Demo.texture_generator import \
     demo_repeat as demo_generate_repeat_texture, \
     demo_trans_folder_all as demo_trans_image_folder_to_all_texture
 
-from texture_synthesis.Demo.texture_manager import \
-    demo as demo_manage_texture, \
-    demo_folder as demo_manage_texture_folder
+from texture_synthesis.Demo.render import demo as demo_render_repeat_texture
 
 if __name__ == "__main__":
     #  demo_generate_repeat_texture()
+
     demo_trans_image_folder_to_all_texture()
 
-    #  demo_manage_texture()
-    #  demo_manage_texture_folder()
+    demo_render_repeat_texture()
