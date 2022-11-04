f17867f3ae758f0f90de6fe9dd80e695b1456441
chLi <565353780@qq.com>
2022-11-04 16:18:30 +0800

    add more new texture render paths

--- texture_synthesis/Demo/render.py
+++ texture_synthesis/Demo/render.py
@@ -12,11 +12,20 @@ def demo():
         '2': '木纹/22-02木纹10/repeat.png',
         '3': '木纹/22-02木纹12/repeat.png',
         '4': '木纹/北美黑胡桃/repeat.png',
+        '5': '木纹/柚木/repeat.png',
+        '6': '木纹/浅胡桃1/repeat.png',
+        '7': '图片/11/repeat.png',
+        '8': '图片/22/repeat.png',
+        '9': '图片/33/repeat.png',
+        '10': '图片/44/repeat.png',
+        '11': '图片/55/repeat.png',
+        '12': '图片/66/repeat.png',
+        '13': '图片/77/repeat.png',
     }
 
-    render_file_idx = 4
+    render_file_idx = 13
     repeat_size = [3, 3]
-    scale = 0.5
+    scale = 0.6
     wait_key = 0
 
     repeat_texture_file_path = \
