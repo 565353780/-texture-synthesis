d7f03f35ada0cd2e3286dcaa127eb3a57c8d3b64
chLi <565353780@qq.com>
2022-12-03 17:23:58 +0800

    simplify demo script

--- demo.py
+++ demo.py
@@ -2,18 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from texture_synthesis.Demo.texture_generator import \
-    demo_repeat as demo_generate_repeat_texture, \
     demo_trans_folder_all as demo_trans_image_folder_to_all_texture
-
 from texture_synthesis.Demo.render import demo as demo_render_repeat_texture
 
-from texture_synthesis.Demo.texture_matcher import demo as demo_match_repeat_texture
-
 if __name__ == "__main__":
-    #  demo_generate_repeat_texture()
-
-    #  demo_match_repeat_texture()
-
     demo_trans_image_folder_to_all_texture()
-
-    demo_render_repeat_texture()
+    #  demo_render_repeat_texture()
