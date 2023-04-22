26fe63d3c79972ec97714808c8cf83c92dd94a00
chLi <565353780@qq.com>
2023-04-22 18:55:25 +0800

    add cut image file func

--- texture_synthesis/Demo/image_cutter.py
+++ texture_synthesis/Demo/image_cutter.py
@@ -5,5 +5,9 @@ from texture_synthesis.Module.image_cutter import ImageCutter
 
 
 def demo():
+    image_file_path = '/home/chli/chLi/test.jpg'
+
     image_cutter = ImageCutter()
+
+    cut_image, mask = image_cutter.cutImageFile(image_file_path)
     return True
--- texture_synthesis/Module/image_cutter.py
+++ texture_synthesis/Module/image_cutter.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import cv2
+
 
 class ImageCutter(object):
     def __init__(self):
         return
+
+    def cutImage(self, image):
+        return cut_image, mask
+
+    def cutImageFile(self, image_file_path):
+        assert os.path.exists(image_file_path)
+
+        image = cv2.imread(image_file_path)
+        return self.cutImage(image)
