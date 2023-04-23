3aeb7915411e0dee3831fa7286f86e764770e52f
chLi <565353780@qq.com>
2023-04-23 17:09:03 +0800

    add more params, add 2 different mode

--- texture_synthesis/Demo/image_cutter.py
+++ texture_synthesis/Demo/image_cutter.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import cv2
 
 from texture_synthesis.Module.image_cutter import ImageCutter
 
 
 def demo():
+    width_expand = 0.1
+    height_expand = 0.1
     image_file_path = '/home/chli/chLi/tt1.png'
 
-    image_cutter = ImageCutter()
+    image_cutter = ImageCutter(width_expand, height_expand)
 
-    data = image_cutter.cutImageFile(image_file_path)
+    mode = 'data'
+    assert mode in ['file', 'data']
+    if mode == 'file':
+        data = image_cutter.cutImageFile(image_file_path)
+    elif mode == 'data':
+        assert os.path.exists(image_file_path)
+        image = cv2.imread(image_file_path)
+        data = image_cutter.cutImage(image)
 
     data = image_cutter.recombineImage(data)
 
